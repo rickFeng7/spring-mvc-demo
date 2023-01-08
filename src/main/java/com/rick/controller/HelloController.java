a82dbe55ea498deea52e6670c8fde3a2d111bddd
fengrui <644561421@qq.com>
2023-01-09 07:10:47 +0800

    init

---
+++ src/main/java/com/rick/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.rick.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author feng4
+ */
+public class HelloController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("hello");
+        mv.addObject("msg", "这是springmvc的第一个程序");
+        return mv;
+    }
+}
